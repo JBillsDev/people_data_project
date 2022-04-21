ec0169c9d71e8de82d17ad3c34f643d97dc34479
Joshua bills <JBillsDev@gmail.com>
2022-04-20 17:51:20 -0700

    v0.0.0b - org.json
    Added JSON library
    Added config file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,19 @@
 import GUI.JFrameWindow;
+import Utility.Config;
 import org.tinylog.Logger;
 
-public class Main {
+final public class Main {
 
     public static void main(String[] args) {
-        final String VERSION = "v0.0.0a";
+        final String VERSION = "v0.0.0b";
         final String TITLE = "People Data Project " + VERSION;
 
         Logger.info(TITLE);
 
-        new JFrameWindow(TITLE, 480, 640);
+        var config = new Config();
+        config.readConfig();
+
+        new JFrameWindow(TITLE, config);
     }
 
 }
